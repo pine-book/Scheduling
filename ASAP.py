79f6bf1e2e69854bd03127068bc91b6df1ecd248
Tatsumi <tatsumi719@gmail.com>
2022-07-06 18:25:09 +0900

    クラスNode、Edgeを作成

--- ASAP.py
+++ ASAP.py
@@ -1,23 +1,39 @@
 import xml.etree.ElementTree as ET
+class Node:
+    id = 0
+    type = ""
 
+    def __init__(self, v):
+        self.id = v.get("id")
+        self.type = v.get("type")
 
-class Graph:
-    id = []
-    type = []
+    def print(self):
+        print(self.id + ":" + self.type)
 
-    def set(v):
-        Graph.id.append(v.get("id"))
-        Graph.type.append(v.get("type"))
-        
-    def print():
-        print(Graph.id)
-        print(Graph.type)
+class Edge:
+    prev = ""
+    next = ""
+
+    def __init__(self, e):
+        self.prev = e.get("prev")
+        self.next = e.get("next")
+
+    def print(self):
+        print(self.prev + ":" + self.next)
 
 tree = ET.parse('graph1.xml')
 
-graph = Graph()
-
 root = tree.getroot()
+
+node = []
 for v in root.findall("V"):
-    Graph.set(v)
-Graph.print()
+    node.append(Node(v))
+for i in range(len(node)):
+    node[i].print()
+
+edge = []
+for e in root.findall("E"):
+    edge.append(Edge(e))
+for i in range(len(edge)):
+    edge[i].print()
+
